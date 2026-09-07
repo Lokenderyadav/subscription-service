e9c56d5c59e9c5b90fc60d3075375132af6d227d
Lokender Yadav Kanneboina <Lokendarkanneboina744@gmail.com>
2026-09-07 10:21:54 -0500

    add subscription created timestamp

--- internal/subscription/model.go
+++ internal/subscription/model.go
@@ -1,8 +1,11 @@
 package subscription
 
+import "time"
+
 type Subscription struct {
-	ID     string
-	UserID string
-	Plan   string
-	Status string
+	ID        string
+	UserID    string
+	Plan      string
+	Status    string
+	CreatedAt time.Time
 }
